34a2b6eb7f1b0119d17141f1505f32a8a266b14f
CJ <128767615+CJRookie@users.noreply.github.com>
2024-04-28 00:47:06 -0400

    Initial Commit

---
+++ ChatAI/ChatAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAIApp.swift
+//  ChatAI
+//
+//  Created by CJ on 4/28/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
